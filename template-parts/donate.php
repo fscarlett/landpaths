fb2cbf2401a9ba592a8f5554f6a318005f8a315f
Fox Scarlett <foxontheone@gmail.com>
2019-09-03 17:13:00 -0700

    add donate button

--- template-parts/donate.php
+++ template-parts/donate.php
@@ -3,7 +3,9 @@
 
 	<h2 class="h1">Donate Today</h2>
 
-	<p>Give $5 today and your money will go to providing knowlegdge to kids.</p>
+	<p>Give $5 today and your money will go to providing knowledge to kids.</p>
+
+	<a href="<?php site_url(); ?>/get-kids-outside/" class="ast-custom-button-link"><button class="ast-custom-button lp-button">Donate</button></a>
 
 
 
